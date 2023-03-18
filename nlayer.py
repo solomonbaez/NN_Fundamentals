1e0325205c05ab3e9095bb76a3ff6e159515c617
Solomon Baez <116975671+solomonbaez@users.noreply.github.com>
2023-03-17 19:20:47 -0600

    Add files via upload
    Added Categorical Cross Entropy error calculation to the nlayer object file. The processing step stacks upon previous ReLU and SoftMax activation.

--- nlayer.py
+++ nlayer.py
@@ -12,7 +12,7 @@ X, y = spiral_data(samples=100, classes=3)
 
 # generally, you will either construct or load a model
 # initial values should be generally be nonzero between (-1, 1)
-class Layer_D:
+class DeepLayer:
     def __init__(self, n_in, n_neurons):
         # init input size by matrix size, no transposition
         self.weights = 0.1*np.random.randn(n_in, n_neurons)
@@ -37,9 +37,26 @@ class SoftMax:
         # normalization per softmax methodology
         self.output = exponentials/np.sum(exponentials, axis=1, keepdims=True)
 
+# Categorical Cross Entropy function, conceptual implementation
+class CCE:
+    def calculate(self, inputs, targets):
+        samples = len(inputs)
+
+        # prevent zero induced process death
+        clipped_targets = np.clip(inputs, 1e-7, 1-1e-7)
+
+        # ensure processing of both scalar and one-hot encoded inputs
+        if len(targets.shape) == 1:
+            confidences = clipped_targets[range(samples), targets]
+        elif len(targets.shape) == 2:
+            confidences = np.sum(clipped_targets*targets, axis=1)
+
+        # calculate and return CCE
+        loss = -np.log(confidences)
+        return np.mean(loss)
 
 # construct the primary layer to process input data
-layer_1 = Layer_D(2, 3)
+layer_1 = DeepLayer(2, 3)
 layer_1.forward(X)
 
 # layer activation using the ReLU method
@@ -47,14 +64,20 @@ activation1 = ReLU()
 activation1.forward(layer_1.output)
 
 # second layer, three classes
-layer_2 = Layer_D(3, 3)
+layer_2 = DeepLayer(3, 3)
 layer_2.forward(activation1.output)
 
 # second layer activation using the SoftMax method
+# probability distribution values are consistent at approximately 0.33
+# expected due to the random initialization of the model
 activation2 = SoftMax()
 activation2.forward(layer_2.output)
 
-# probability distribution values are consistent at approximately 0.33
-# expected due to the random initialization of the model
-print(activation2.output)
+# Categorical Cross Entropy loss calculation
+# SoftMax processed data is passed as the input, y as the target
+cce = CCE()
+loss = cce.calculate(activation2.output, y)
+
+print(loss)
+
 
